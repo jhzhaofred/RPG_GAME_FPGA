956cb7656e0eef977819eb5d9ea0ee03af125aee
zaazad <34041655+zaazad@users.noreply.github.com>
2018-04-20 19:19:37 -0400

    Add files via upload

--- game_engine.v
+++ game_engine.v
@@ -1,88 +1,194 @@
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer:
-//
-// Create Date:   19:09:16 04/20/2018
-// Design Name:   game_engin
-// Module Name:   X:/Desktop/RPG_GAME_FPGA-master/RPG_GAME_FPGA-master/RPG/TB_gameengine.v
-// Project Name:  RPG
-// Target Device:  
-// Tool versions:  
+// Engineer: 
+// 
+// Create Date:    21:04:00 03/24/2018 
+// Design Name: 
+// Module Name:    game_engin 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: game_engin
+// Dependencies: 
 //
-// Dependencies:
-// 
-// Revision:
+// Revision: 
 // Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module game_engin( input clk,
+						 input collision_detected, // to start the battle
+						 input [1:0] player_choice, // to choose attack type
+						 input [1:0] enemy_choice,
+						 input  player_turn,//it's decided based on the input keys 
+						 input  attacker_turn,
+						 output reg [7:0] player_HB, //player's health condition to be shown on the monitor
+						 output reg [7:0] enemy_HB,
+						 output reg [4:0] player_remained_sword,//punchs and kicks are infinite 
+						 output reg [4:0] player_remained_baseballbat,//needed to be shown on the screen
+						 output reg [4:0] enemy_remained_sword,
+						 output reg [4:0] enemy_remained_baseballbat,
+						 output reg player_win,
+						 output reg enemy_win
+    );
 
-module TB_gameengine;
 
-	// Inputs
-	reg clk;
-	reg collision_detected;
-	reg [1:0] player_choice;
-	reg [1:0] enemy_choice;
-	reg player_turn;
-	reg attacker_turn;
+parameter P=2'b00, K=2'b01, S=2'b10, B=2'b11;
+reg [7:0] pun_var, kick_var, bat_var, sword_var; 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////These initial numbers can be changed/////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// Outputs
-	wire [7:0] player_HB;
-	wire [7:0] enemy_HB;
-	wire [4:0] player_remained_sword;
-	wire [4:0] player_remained_baseballbat;
-	wire [4:0] enemy_remained_sword;
-	wire [4:0] enemy_remained_baseballbat;
-	wire player_win;
-	wire enemy_win;
+initial
+begin
+player_HB = 8'd100;
+enemy_HB = 8'd100;
+end
 
-	// Instantiate the Unit Under Test (UUT)
-	game_engin uut (
-		.clk(clk), 
-		.collision_detected(collision_detected), 
-		.player_choice(player_choice), 
-		.enemy_choice(enemy_choice), 
-		.player_turn(player_turn), 
-		.attacker_turn(attacker_turn), 
-		.player_HB(player_HB), 
-		.enemy_HB(enemy_HB), 
-		.player_remained_sword(player_remained_sword), 
-		.player_remained_baseballbat(player_remained_baseballbat), 
-		.enemy_remained_sword(enemy_remained_sword), 
-		.enemy_remained_baseballbat(enemy_remained_baseballbat), 
-		.player_win(player_win), 
-		.enemy_win(enemy_win)
-	);
 
-	initial begin
-		// Initialize Inputs
-		clk = 0;
-		collision_detected = 0;
-		player_choice = 0;
-		enemy_choice = 0;
-		player_turn = 0;
-		attacker_turn = 0;
+initial
+begin 
+player_remained_sword = 5'd4;
+player_remained_baseballbat = 5'd3;
 
-		// Wait 100 ns for global reset to finish
-		#100;
-        player_choice = 2'b11;
-		  collision_detected = 1;
-		  player_turn = 1;
-		// Add stimulus here
-		
-		#100
-			enemy_choice = 1;
-			enemy_choice = 2'b10;
-	end
+enemy_remained_sword = 5'd4;
+enemy_remained_baseballbat = 5'd3;
+end
+
+//Full Health Bar is 100
+//We need to deciede on these numbers
+//assign Punch_Accuracy = 100; 
+//assign Puch_Power = 5; 
+//assign Kick_Accuracy = 80;
+//assign Kick_Power = 10;
+//assign Sword_Accuracy = 40;
+//assign Sword_Power = 20;
+//assign BaseballBat_Accuracy = 30;
+//assign BaseballBat_Power = 40;
+
+//Strength levels should have overlap to to prevent the attackers from using different attack types in their power order
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+always @(posedge clk)
+begin
+
+
+pun_var = $random%3;
+kick_var = $random%5;
+bat_var = $random%7;
+sword_var = $random%9;
 	
-	always 
-	#5 clk = !clk;
-      
+if (player_HB == 0)
+		enemy_win = 1'b1;
+else if (enemy_HB == 0)
+		player_win = 1'b1;
+end
+///////////////////////////////////////////////////////////////////////////////////////////////////
+always @(posedge clk)
+begin
+if(collision_detected == 1)
+begin
+if (player_turn == 1)
+begin
+		case (player_choice)
+		
+			P: begin
+				
+					if (pun_var > 8'h02)
+						enemy_HB = enemy_HB - ( 8'd10 - (~pun_var + 1));
+					else
+						enemy_HB = enemy_HB -( 8'd10 + pun_var); // 8-12
+			end
+			K:begin
+					if (bat_var > 8'h06)
+						enemy_HB = enemy_HB -( 8'd30 - (~bat_var + 1));
+					else
+						enemy_HB = enemy_HB - (8'd30 + bat_var); // 24-36
+
+			end
+			S:begin
+				if (player_remained_sword > 0)
+				begin
+					if (sword_var > 8'h08)
+						enemy_HB = enemy_HB -( 8'd40 - (~sword_var + 1));
+					else
+						enemy_HB = enemy_HB - ( 8'd40 + sword_var); // 32-48
+					player_remained_sword = player_remained_sword - 1;
+					
+				end
+			end
+			
+			B:begin
+				if (player_remained_baseballbat > 0)
+				begin
+					if (sword_var > 8'h08)
+						enemy_HB = enemy_HB -( 8'd40 - (~sword_var + 1));
+					else
+						enemy_HB = enemy_HB - ( 8'd40 + sword_var); // 32-48
+				end
+					player_remained_baseballbat = player_remained_baseballbat - 1;
+
+				end
+		endcase
+end
+end
+end
+//////////////////////////////////////some parts of the previous always block should be added here/////////////////////
+always @(posedge clk)
+begin
+if(collision_detected == 1)
+begin
+if (attacker_turn == 1)
+begin
+		case (enemy_choice)
+		
+			P: begin
+					if (pun_var > 8'h02)
+						player_HB = player_HB - ( 8'd10 - (~pun_var + 1));
+					else
+						player_HB = player_HB -( 8'd10 + pun_var); // 8-12
+					 
+			end
+			K:begin
+			
+					if (bat_var > 8'h06)
+						player_HB = player_HB -( 8'd30 - (~bat_var + 1));
+					else
+						player_HB = player_HB - (8'd30 + bat_var); // 24-36
+				
+			end
+			S:begin
+				if (enemy_remained_sword > 0)
+				begin
+					if (sword_var > 8'h08)
+						player_HB = player_HB - ( 8'd40 - (~sword_var + 1));
+					else
+						player_HB = player_HB - ( 8'd40 + sword_var); // 32-48
+					enemy_remained_sword = enemy_remained_sword - 1;
+					
+				end
+			end
+			
+			B:begin
+				if (enemy_remained_baseballbat > 0)
+				begin
+					if (sword_var > 8'h08)
+						player_HB = player_HB -( 8'd40 - (~sword_var + 1));
+					else
+						player_HB = player_HB - ( 8'd40 + sword_var); // 32-48
+				end
+					enemy_remained_baseballbat = enemy_remained_baseballbat - 1;
+
+				end
+		endcase
+end
+end
+end
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 endmodule
 
